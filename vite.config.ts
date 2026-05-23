82fee09d377a519849bab1dbc2c8d1ce62b90e7e
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2026-05-23 19:07:06 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   build: {
+    outDir: "admin.dpn-pppi.org",
     rollupOptions: {
       external: [],
     },
